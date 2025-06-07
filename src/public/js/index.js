e8eae5dff942d2b105dee03eb978639da0a64f61
GABRIEL YAGO RODRIGUES LEAL <gabriel.yrleal@senacsp.edu.br>
2025-06-06 21:01:39 -0300

    atualizado

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,3 +1,10 @@
 document.getElementById('btnEntrar').addEventListener('click', () => {
   window.location.href = "cadastro.html";
 });
+function abrirEmpresa() {
+  window.location.href = "empresa.html";
+}
+
+function abrirOS() {
+  window.location.href = "os.html";
+}
--- src/views/renderer.js
+++ src/views/renderer.js
@@ -7,3 +7,14 @@ ipcRenderer.on('abrir-relatorio-clientes', () => {
 ipcRenderer.on('abrir-sobre', () => {
   alert('Sistema OC Nobreaks\nVersão 1.0\nDesenvolvido por Gabriel Yago');
 });
+
+// Troca do ícone do banco de dados (usando a api do preload.js)
+api.dbStatus((event, message) => {
+  //teste do recebimento da mensagem do main
+  console.log(message)
+  if (message === "conectado") {
+      document.getElementById('statusdb').src = "../public/img/dbon.png"
+  } else {
+      document.getElementById('statusdb').src = "../public/img/dboff.png"
+  }
+})
